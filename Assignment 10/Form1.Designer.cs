eb8853ae537563ea4e755d1d0ff569bd215ad2be
Ucar <rucar@boilerad.purdue.edu>
2018-11-14 21:33:44 -0500

    Added project files

---
+++ Assignment 10/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Assignment_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtScore = new System.Windows.Forms.TextBox();
+            this.chkDean = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radDesc = new System.Windows.Forms.RadioButton();
+            this.radAsc = new System.Windows.Forms.RadioButton();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lstOut = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 36);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(38, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name:";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(12, 74);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(38, 13);
+            this.lblScore.TabIndex = 1;
+            this.lblScore.Text = "Score:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(94, 33);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtScore
+            // 
+            this.txtScore.Location = new System.Drawing.Point(94, 71);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(100, 20);
+            this.txtScore.TabIndex = 3;
+            // 
+            // chkDean
+            // 
+            this.chkDean.AutoSize = true;
+            this.chkDean.Location = new System.Drawing.Point(94, 122);
+            this.chkDean.Name = "chkDean";
+            this.chkDean.Size = new System.Drawing.Size(84, 17);
+            this.chkDean.TabIndex = 4;
+            this.chkDean.Text = "Dean\'s List?";
+            this.chkDean.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radDesc);
+            this.groupBox1.Controls.Add(this.radAsc);
+            this.groupBox1.Location = new System.Drawing.Point(250, 59);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(124, 102);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sort Order";
+            // 
+            // radDesc
+            // 
+            this.radDesc.AutoSize = true;
+            this.radDesc.Location = new System.Drawing.Point(10, 63);
+            this.radDesc.Name = "radDesc";
+            this.radDesc.Size = new System.Drawing.Size(82, 17);
+            this.radDesc.TabIndex = 1;
+            this.radDesc.TabStop = true;
+            this.radDesc.Text = "Descending";
+            this.radDesc.UseVisualStyleBackColor = true;
+            // 
+            // radAsc
+            // 
+            this.radAsc.AutoSize = true;
+            this.radAsc.Location = new System.Drawing.Point(10, 30);
+            this.radAsc.Name = "radAsc";
+            this.radAsc.Size = new System.Drawing.Size(75, 17);
+            this.radAsc.TabIndex = 0;
+            this.radAsc.TabStop = true;
+            this.radAsc.Text = "Ascending";
+            this.radAsc.UseVisualStyleBackColor = true;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(18, 155);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(95, 30);
+            this.btnEnter.TabIndex = 6;
+            this.btnEnter.Text = "&Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(131, 155);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(99, 30);
+            this.btnDisplay.TabIndex = 7;
+            this.btnDisplay.Text = "&Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(275, 203);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(75, 23);
+            this.btnSort.TabIndex = 8;
+            this.btnSort.Text = "Sort - Score";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(275, 232);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(275, 261);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lstOut
+            // 
+            this.lstOut.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstOut.FormattingEnabled = true;
+            this.lstOut.Location = new System.Drawing.Point(18, 203);
+            this.lstOut.Name = "lstOut";
+            this.lstOut.Size = new System.Drawing.Size(236, 82);
+            this.lstOut.TabIndex = 11;
+            this.lstOut.SelectedIndexChanged += new System.EventHandler(this.lstOut_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 301);
+            this.Controls.Add(this.lstOut);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.chkDean);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
+            this.Text = "Assignment 10 By Resul Ucar";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtScore;
+        private System.Windows.Forms.CheckBox chkDean;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radDesc;
+        private System.Windows.Forms.RadioButton radAsc;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ListBox lstOut;
+    }
+}
+
---
+++ Assignment 10/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+/*=========================================================
+* Resul Ucar
+* rucar@purdue.edu
+* CNIT155 Assignment10
+* Lab Section: Fr. 03:30
+* Program Description: 
+*
+* Academic Honesty: 
+*	I attest that this is my original work.
+*	I have not used unauthorized source code, either modified or unmodified.
+*	I have not given other fellow student(s) access to my program.
+*=========================================================== */
+namespace Assignment_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private const int cSize = 100;
+        private string[] mName = new string[cSize];
+        private int[] mScore = new int[cSize];
+        private bool[] mDean = new bool[cSize];
+        private int mIndex = 0;
+        private string mFileName = Path.Combine(Application.StartupPath, "Students.txt");
+        
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            if (ValidateInput() == false)
+            {
+                return;
+            }
+            string name = txtName.Text;
+            int score = int.Parse(txtScore.Text);
+            mName[mIndex] = name;
+            mScore[mIndex] = score;
+            if (chkDean.Checked == true)
+            {
+                mDean[mIndex] = true;
+            }
+            if (chkDean.Checked == false)
+            {
+                mDean[mIndex] = false;
+            }
+            mIndex++;
+            if (mIndex == cSize)
+            {
+                DisplayMessage("The array is full!");
+                btnEnter.Enabled = false;
+            }
+            ClearInput();
+        }
+        private void ClearInput()
+        {
+            txtName.Clear();
+            txtScore.Clear();
+            chkDean.Checked = false;
+            txtName.Focus();
+            int index = lstOut.SelectedIndex;
+        }
+        private void DisplayMessage(String msg)
+        {
+            MessageBox.Show(msg, Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private bool ValidateInput()
+        {
+            string name = txtName.Text;
+            int score;
+            if (name == " ")
+            {
+                DisplayMessage("Please enter a name");
+                txtName.Focus();
+                return false;
+            }
+            if(int.TryParse(txtScore.Text, out score)==false)
+            {
+                DisplayMessage("Please enter a whole number");
+                txtScore.Focus();
+                return false;
+            }
+            if(score<0||score>100)
+            {
+                DisplayMessage("Please enter a score between 0 to 100");
+                txtScore.Focus();
+                return false;
+            }
+
+            return true;
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            if (mIndex == 0)
+            {
+                DisplayMessage("The array is empty!");
+                return;
+            }
+            lstOut.Items.Clear();
+            int ctr;
+            lstOut.Items.Add("Name".PadRight(20) + "Score".PadRight(10) + "Dean's List");
+            lstOut.Items.Add("=============================================================");
+            for (ctr = 0; ctr < mIndex; ctr++)
+            {
+                lstOut.Items.Add(mName[ctr].PadRight(20) + mScore[ctr].ToString().PadRight(10)+mDean[ctr]);
+            }
+        }
+       
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Do you wish to end the program?", Text, MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+            {
+                e.Cancel = true;
+                return;
+            }
+            StreamWriter SW = null;
+            try
+            {
+                SW = new StreamWriter(mFileName);
+                int ctr;
+                for (ctr = 0; ctr < mIndex; ctr++)
+                {
+                    SW.WriteLine((ctr + 1) + "\t" + mName[ctr] + "\t" + mScore[ctr] + "\t" + mDean[ctr]);
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+            finally
+            {
+                {
+                    if (SW != null)
+                        SW.Close();
+                }
+
+            }
+
+
+        }
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearInput();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void lstOut_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(lstOut.SelectedIndex <2)
+            {
+                return;
+            }
+            int index = lstOut.SelectedIndex - 2;
+            txtName.Text = mName[index];
+            txtScore.Text = mScore[index].ToString();
+            if (mDean[index] == false)
+            {
+                chkDean.Checked = false;
+            }
+            else
+                chkDean.Checked = true;
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            if (mIndex == 0)
+            {
+                DisplayMessage("The arrays are empty.");
+                return;
+            }
+            if (radAsc.Checked == false && radDesc.Checked == false)
+            {
+                DisplayMessage("Please select a sort option.");
+                return;
+            }
+
+            lstOut.Items.Clear();
+
+            int[] Score1 = new int[cSize];
+            for(int count = 0; count <mIndex; count++)
+            {
+                Score1[count] = mScore[count];
+            }
+            Array.Sort(mScore, mName, 0, mIndex);
+            Array.Sort(Score1, mDean, 0, mIndex);
+            if (radDesc.Checked==true)
+            {
+                Array.Reverse(mName, 0, mIndex);
+                Array.Reverse(mScore, 0, mIndex);
+                Array.Reverse(mDean, 0, mIndex);
+            }
+
+            lstOut.Items.Add("Name".PadRight(10) + "Score".PadRight(10) + "Dean's List");
+            lstOut.Items.Add("=================================================================");
+            for (int count = 0; count < mIndex; count++)
+            { 
+                lstOut.Items.Add(mName[count].PadRight(10) + mScore[count].ToString().PadRight(10) + mDean[count].ToString());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(mFileName) == false)
+
+            {
+
+                DisplayMessage("The file does not exist.");
+
+                return;
+
+            }
+
+            StreamReader SR = null;
+
+            try
+
+            {
+
+                SR = new StreamReader(mFileName);
+
+                while (SR.Peek() != -1)
+
+                {
+
+                    string line = SR.ReadLine();
+
+                    string[] parts = line.Split('\t');
+
+                    mName[mIndex] = parts[1];
+
+                    mScore[mIndex] = int.Parse(parts[2]);
+
+                    if (parts[3] == "True")
+
+                    {
+
+                        mDean[mIndex] = true;
+
+                    }
+
+                    else
+
+                    {
+
+                        mDean[mIndex] = false;
+
+                    }
+
+                    mIndex++;
+
+                }
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                MessageBox.Show(ex.Message, Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+
+            finally
+
+            {
+
+                SR.Close();
+
+            }
+        }
+    }
+}
